f38eb21c4868ff827efb382e9ef1130f55695f28
nawaf <n.alhu10@gmail.com>
2024-04-03 03:57:39 +0300

    write a method membership

--- Membership.py
+++ Membership.py
@@ -1,3 +1,21 @@
 class Membership:
-    def __init__(self, ):
-        pass
+    def __init__(self, level):
+        self.level = level
+        self.benefits = {}
+
+    def getLevel(self):
+        return self.level
+    
+    def setLevel(self, level):
+        self.level = level
+
+    def getBenefits(self):
+        return self.benefits
+    
+    def addBenefits(self, benefit):
+        self.benefits[benefit] = True
+        return self.benefits
+
+    def removeBenefits(self, benefit):
+        if benefit in self.benefits.keys():
+            del self.benefits[benefit]
