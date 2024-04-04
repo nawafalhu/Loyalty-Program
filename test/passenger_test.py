1f8745ae4179bbf0c392c257cfa99c980cdeadf8
nawaf <n.alhu10@gmail.com>
2024-04-04 05:12:58 +0300

    write a test code for passenger class

--- test/passenger_test.py
+++ test/passenger_test.py
@@ -1,13 +1,23 @@
 from src.passenger import Passenger
+from src.flight import Flight
+from src.membership import Membership
 
-def test_add_miles():
-    pass
+membership = Membership("silver", 2)
+flight = Flight("SA112", "RUH", "NYC", 11100, "1:00PM", "2:30AM")
+passenger = Passenger("nawaf", 1122, "economy", membership, flight)
 
-def test_redeem_miles():
-    pass
+def get_set_SeatClass_test():
+    assert passenger.getSeatClass() == "economy"
+    assert passenger.setSeatClass("business") == "business"
 
-def test_get_free_ticket():
-    pass
+def get_name_id_miles_test():
+    assert passenger.getId() == 1122
+    assert passenger.getName() == "nawaf"
+    assert passenger.getMiles() == 0
 
-def test_get_set_membershiplevel():
-    pass
+def calculate_miles_test():
+    assert passenger.calculateMiles() == 11100 * 0.75
+
+def get_FreeTicket_test():
+    passenger.calculateMiles()
+    assert passenger.getFreeTicket() == "You can get 0 international flights, and 0 domestic flight for free!"
