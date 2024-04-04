fac1e35acd83818726d0ebbf427b4e731397b86a
nawaf <n.alhu10@gmail.com>
2024-04-04 23:06:36 +0300

    change class reward

--- src/reward.py
+++ src/reward.py
@@ -2,57 +2,29 @@ from abc import ABC, abstractmethod
 
 class Reward(ABC):
     @abstractmethod
-    def FreeDomesticFlightTicket(self, miles):
-        if miles >= 9000:
-            return True
-        return False
-
-    @abstractmethod
-    def FreeInternationalFlightTicket(self, miles):
-        if miles >= 10000:
-            return True
-        return False
-    
-    @abstractmethod
-    def udgradeSeat(self):
-        pass
-
-    @abstractmethod
-    def addExtraBaggage(self):
+    def redeem(self, membership):
         pass
 
 # basic level
-class basic(Reward):
-    def udgradeSeat(self, miles):
-        if miles >= 3000:
-            return True
-        return False
+class FreeDomesticFlightTicket(Reward):
+    def redeem(self, membership):
+        pass
 
-    def addExtraBaggage(self, miles):
-        if miles >= 1500:
-            return True
-        return False
 
 # silver level
-class silver(Reward):    
-    def udgradeSeat(self, miles):
-        if miles >= 2500:
-            return True
-        return False
-
-    def addExtraBaggage(self, miles):
-        if miles >= 1000:
-            return True
-        return False
+class FreeInternationalFlightTicket(Reward):
+    def redeem(self, membership):
+        pass
 
 # gold level
-class gold(Reward):
-    def udgradeSeat(self, miles):
-        if miles >= 2000:
-            return True
-        return False
+class udgradeSeat(Reward):
+    def redeem(self, membership):
+        pass
 
-    def addExtraBaggage(self, miles):
-        if miles >= 500:
-            return True
-        return False
+
+
+
+# FreeDomesticFlightTicket
+# FreeInternationalFlightTicket
+# udgradeSeat
+# addExtraBaggage
