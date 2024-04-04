b91242525c16b3c30a5d41f602c868eed2f581d3
nawaf <n.alhu10@gmail.com>
2024-04-04 19:55:02 +0300

    write a method upgrade seat

--- src/reward.py
+++ src/reward.py
@@ -2,21 +2,49 @@ from abc import ABC, abstractmethod
 
 class Reward(ABC):
     @abstractmethod
-    def method1(self):
-        pass
+    def FreeDomesticFlightTicket(self, miles):
+        if miles >= 9000:
+            return True
+        return False
+
+    @abstractmethod
+    def FreeInternationalFlightTicket(self, miles):
+        if miles >= 10000:
+            return True
+        return False
     
     @abstractmethod
-    def method2(self):
+    def udgradeSeat(self):
         pass
+
+    @abstractmethod
+    def addExtraBaggage(self):
+        pass
+
 
 class basic(Reward):
-    def __init__(self):
+    def udgradeSeat(self, miles):
+        if miles >= 3000:
+            return True
+        return False
+
+    def addExtraBaggage(self, miles):
+        if miles >= 1500:
+            return True
+        return False
+
+
+class silver(Reward):    
+    def udgradeSeat(self):
         pass
 
-class silver(Reward):
-    def __init__(self):
+    def addExtraBaggage(self):
         pass
+
 
 class gold(Reward):
-    def __init__(self):
+    def udgradeSeat(self):
+        pass
+
+    def addExtraBaggage(self):
         pass
